f5ed01e2c56d9c43842a36e82d60b09c50ae8ad2
psuda87 <sudaaa@gmail.com>
2024-12-11 00:16:14 +0100

    Naprawa bledu laczenia z baza dziala logowanie uzytkownika i tworzenie nowego uzytkownika

--- Paragoniarz/DatabaseHelper.cs
+++ Paragoniarz/DatabaseHelper.cs
@@ -1,107 +1,26 @@
-﻿using MySql.Data.MySqlClient;
-using System;
-using System.Windows.Forms;
+﻿using System;
+using System.Data.SqlClient;
 using System.Security.Cryptography;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Paragoniarz
 {
     public class DatabaseHelper
     {
-        // Zmienna connectionString, używana w całej klasie
-        string connectionString = "Server=paragoniarz.database.windows.net;Port=1433;Database=paragoniarz_db;Uid=p4ragoniarzadmin;Pwd=zaq1@WSX123;SslMode=Required;";
-
-
-        // Testowe połączenie z bazą danych
-        //public void TestConnection()
-        //{
-        //    using (MySqlConnection conn = new MySqlConnection(connectionString))
-        //    {
-        //        try
-        //        {
-        //            conn.Open();
-        //            Console.WriteLine("Połączenie udane!");
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            Console.WriteLine($"Błąd połączenia: {ex.Message}");
-        //        }
-        //    }
-        //}
-
-        public void TestConnection()
-        {
-            Console.WriteLine("Przygotowanie do nawiązania połączenia...");
-
-            using (MySqlConnection conn = new MySqlConnection(connectionString))
-            {
-                try
-                {
-                    Console.WriteLine("Sprawdzanie poprawności connection stringa...");
-                    if (string.IsNullOrEmpty(connectionString))
-                    {
-                        Console.WriteLine("Błąd: Connection string jest pusty lub null!");
-                        return;
-                    }
-
-                    Console.WriteLine("Connection string: " + connectionString);
-                    Console.WriteLine("Rozpoczynanie otwierania połączenia...");
-
-                    conn.Open();
-                    Console.WriteLine("Połączenie otwarte pomyślnie!");
-
-                    Console.WriteLine("Sprawdzanie statusu połączenia...");
-                    if (conn.State == System.Data.ConnectionState.Open)
-                    {
-                        Console.WriteLine("Połączenie z bazą danych jest aktywne.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Połączenie z bazą danych nie zostało otwarte.");
-                    }
-                }
-                catch (MySqlException mysqlEx)
-                {
-                    Console.WriteLine($"MySqlException: {mysqlEx.Message}");
-                    Console.WriteLine($"Kod błędu MySql: {mysqlEx.Number}");
-                    Console.WriteLine($"Źródło błędu: {mysqlEx.Source}");
-                }
-                catch (InvalidOperationException invalidOpEx)
-                {
-                    Console.WriteLine($"InvalidOperationException: {invalidOpEx.Message}");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Nieznany błąd: {ex.Message}");
-                }
-                finally
-                {
-                    Console.WriteLine("Próba zamknięcia połączenia...");
-                    if (conn.State == System.Data.ConnectionState.Open)
-                    {
-                        conn.Close();
-                        Console.WriteLine("Połączenie zamknięte.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Połączenie było już zamknięte lub nigdy nie zostało otwarte.");
-                    }
-                }
-            }
-        }
-
-
+        // Connection string do SQL Server
+        string connectionString = "Server=tcp:paragoniarz.database.windows.net,1433;Initial Catalog=paragoniarz_db;Persist Security Info=False;User ID=p4ragoniarzadmin;Password=zaq1@WSX123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
         // Sprawdzanie, czy użytkownik lub email są już zajęte
         public bool IsUsernameOrEmailTaken(string username,string email)
         {
-            using (var connection = new MySqlConnection(connectionString))
+            using (var connection = new SqlConnection(connectionString))
             {
                 try
                 {
                     connection.Open();
-                    string query = "SELECT COUNT(*) FROM users WHERE username = @username OR email = @email";
-                    using (var cmd = new MySqlCommand(query,connection))
+                    string query = "SELECT COUNT(*) FROM dbo.Users WHERE username = @username OR email = @email";
+                    using (var cmd = new SqlCommand(query,connection))
                     {
                         cmd.Parameters.AddWithValue("@username",username);
                         cmd.Parameters.AddWithValue("@email",email);
@@ -113,7 +32,7 @@ namespace Paragoniarz
                 catch (Exception ex)
                 {
                     Console.WriteLine("Błąd: " + ex.Message);
-                    return false; // W przypadku błędu zakłada, że użytkownik nie istnieje
+                    return false;
                 }
             }
         }
@@ -123,17 +42,16 @@ namespace Paragoniarz
         {
             string hashedPassword = HashPassword(password); // Haszowanie hasła
 
-            string query = "INSERT INTO dbo.users (username, password, email) VALUES (@username, @password, @email)";
+            string query = "INSERT INTO dbo.Users (username, password, email) VALUES (@username, @password, @email)";
 
-            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 try
                 {
                     conn.Open();
-                    MySqlCommand cmd = new MySqlCommand(query,conn);
+                    SqlCommand cmd = new SqlCommand(query,conn);
                     cmd.Parameters.AddWithValue("@username",username);
-                    
-                    cmd.Parameters.AddWithValue("@password",hashedPassword); // Hasło zahaszowane
+                    cmd.Parameters.AddWithValue("@password",hashedPassword);
                     cmd.Parameters.AddWithValue("@email",email);
 
                     int result = cmd.ExecuteNonQuery();
@@ -158,10 +76,8 @@ namespace Paragoniarz
         {
             using (SHA256 sha256Hash = SHA256.Create())
             {
-                // Konwertowanie danych na tablicę bajtów
                 byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
 
-                // Konwertowanie tablicy bajtów na ciąg
                 StringBuilder builder = new StringBuilder();
                 for (int i = 0; i < bytes.Length; i++)
                 {
@@ -174,23 +90,21 @@ namespace Paragoniarz
         // Walidacja użytkownika
         public bool ValidateUser(string username,string password)
         {
-            string hashedPassword = HashPassword(password); // Haszowanie hasła
-            MessageBox.Show($"Hashed password: {hashedPassword}"); // Wyświetl hash, aby upewnić się, że jest poprawny
+            string hashedPassword = HashPassword(password);
 
-            string query = "SELECT COUNT(*) FROM Users WHERE username = @username AND password = @password";
-            MessageBox.Show($"Query: {query}"); // Wyświetl zapytanie SQL
+            string query = "SELECT COUNT(*) FROM dbo.Users WHERE username = @username AND password = @password";
 
-            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 try
                 {
                     conn.Open();
-                    MySqlCommand cmd = new MySqlCommand(query,conn);
+                    SqlCommand cmd = new SqlCommand(query,conn);
                     cmd.Parameters.AddWithValue("@username",username);
-                    cmd.Parameters.AddWithValue("@password",hashedPassword); // Weryfikacja hasła w postaci haszowanej
+                    cmd.Parameters.AddWithValue("@password",hashedPassword);
 
                     int count = Convert.ToInt32(cmd.ExecuteScalar());
-                    return count > 0; // Jeśli znaleziono użytkownika, zwróci true
+                    return count > 0;
                 }
                 catch (Exception ex)
                 {
@@ -199,6 +113,5 @@ namespace Paragoniarz
                 }
             }
         }
-
     }
 }
