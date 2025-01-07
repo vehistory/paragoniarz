d1a1384fe07ea254187bfdd6a9f4daf9126762cb
bascioTOja <hjhjg55sajda@interia.pl>
2025-01-07 17:13:11 +0100

    Initial full text search

--- Paragoniarz/YourFilesControl.Designer.cs
+++ Paragoniarz/YourFilesControl.Designer.cs
@@ -28,115 +28,182 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.textBoxPhrase = new System.Windows.Forms.TextBox();
+            this.textBoxPerson = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableFiles = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label1.Location = new System.Drawing.Point(282, 398);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(478, 86);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "YourFileCOntrol";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(13, 31);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(90, 32);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Nazwa";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(138, 31);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 32);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Data";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.Location = new System.Drawing.Point(237, 31);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(247, 32);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Wygenerowany opis";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label5.Location = new System.Drawing.Point(522, 31);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(179, 32);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Załączony plik";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label6.Location = new System.Drawing.Point(742, 31);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(108, 32);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "Rozmiar";
-            // 
+            //
+            // tableLayoutPanel1
+            //
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableFiles, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 74.5F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1003, 600);
+            this.tableLayoutPanel1.TabIndex = 0;
+            //
             // panel1
-            // 
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label6);
-            this.panel1.Controls.Add(this.label3);
+            //
+            this.panel1.Controls.Add(this.textBoxPhrase);
+            this.panel1.Controls.Add(this.textBoxPerson);
+            this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label4);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(984, 92);
-            this.panel1.TabIndex = 6;
-            // 
+            this.panel1.Size = new System.Drawing.Size(997, 147);
+            this.panel1.TabIndex = 0;
+            //
+            // textBoxPhrase
+            //
+            this.textBoxPhrase.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBoxPhrase.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxPhrase.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.textBoxPhrase.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.textBoxPhrase.Location = new System.Drawing.Point(326, 52);
+            this.textBoxPhrase.Name = "textBoxPhrase";
+            this.textBoxPhrase.Size = new System.Drawing.Size(434, 22);
+            this.textBoxPhrase.TabIndex = 25;
+            this.textBoxPhrase.Tag = "";
+            //
+            // textBoxPerson
+            //
+            this.textBoxPerson.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBoxPerson.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxPerson.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.textBoxPerson.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.textBoxPerson.Location = new System.Drawing.Point(24, 52);
+            this.textBoxPerson.Name = "textBoxPerson";
+            this.textBoxPerson.Size = new System.Drawing.Size(278, 22);
+            this.textBoxPerson.TabIndex = 24;
+            this.textBoxPerson.Tag = "";
+            //
+            // button3
+            //
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.SystemColors.Highlight;
+            this.button3.FlatAppearance.BorderSize = 3;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.ForeColor = System.Drawing.SystemColors.Menu;
+            this.button3.Location = new System.Drawing.Point(795, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(169, 35);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "Wyszukaj";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            //
+            // label5
+            //
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Black", 18F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label5.Location = new System.Drawing.Point(320, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 32);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Fraza";
+            //
+            // label4
+            //
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Black", 18F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label4.Location = new System.Drawing.Point(18, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 32);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Osoba";
+            //
+            // label2
+            //
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Black", 18F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label2.Location = new System.Drawing.Point(787, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 32);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Data dodania";
+            //
+            // label1
+            //
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 18F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label1.Location = new System.Drawing.Point(3, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 32);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Nazwa pliku";
+            //
+            // tableFiles
+            //
+            this.tableFiles.AutoSize = true;
+            this.tableFiles.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableFiles.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableFiles.ColumnCount = 3;
+            this.tableFiles.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableFiles.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 170F));
+            this.tableFiles.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.tableFiles.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableFiles.Location = new System.Drawing.Point(3, 156);
+            this.tableFiles.Name = "tableFiles";
+            this.tableFiles.RowCount = 1;
+            this.tableFiles.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.tableFiles.Size = new System.Drawing.Size(997, 44);
+            this.tableFiles.TabIndex = 1;
+            //
             // YourFilesControl
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            //
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "YourFilesControl";
-            this.Size = new System.Drawing.Size(984, 561);
+            this.Size = new System.Drawing.Size(1003, 600);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBoxPerson;
+        private System.Windows.Forms.TextBox textBoxPhrase;
+        private System.Windows.Forms.TableLayoutPanel tableFiles;
     }
 }
